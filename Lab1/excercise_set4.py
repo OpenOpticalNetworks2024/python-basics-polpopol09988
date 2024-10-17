5da860f0feaffbba2286a04535330406fb7877aa
polpopol0 <molino.paolo2001@gmail.com>
2024-10-17 17:45:02 +0200

    finitooo
    fine tutto

--- Lab1/excercise_set4.py
+++ Lab1/excercise_set4.py
@@ -1,3 +1,30 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+df = pd.read_csv("resources\sales_data.csv")
+print(df.keys())
+profitlist = df['total_profit'].tolist()
+monthlist = df['month_number'].tolist()
+toothpastelist = df['toothpaste'].tolist()
+bathingsoaplist = df['bathingsoap'].tolist()
+facecreamlist = df['facecream'].tolist()
+
+plt.plot(monthlist,profitlist,'r-',monthlist,facecreamlist,'g-',linewidth =3,marker='o',markerfacecolor='k')
+plt.ylabel("Profit las year")
+plt.savefig("prova.png")
+#plt.show()
+
+plt.scatter(monthlist,toothpastelist)
+#plt.show()
+
+plt.bar(monthlist,bathingsoaplist)
+#plt.show()
+
+plt.hist(profitlist)
+#plt.show()
+
+
+
 
--- Lab1/excercise_set5.py
+++ Lab1/excercise_set5.py
@@ -1,2 +1,37 @@
 import numpy as np
 import matplotlib as plt
+import json
+
+# 1
+# JSON data as a string
+json_data = '{"name": "John", "age": 30, "city": "New York"}'
+# Convert JSON data to Python object (dictionary)
+python_obj = json.loads(json_data)
+# print(python_obj)
+
+# 2
+python_obj = {"name": "John", "age": 30, "city": "New York"}
+# Convert Python object to JSON data
+json_data = json.dumps(python_obj)
+# print(json_data)
+
+# 4
+python_obj = {"city": "New York", "name": "John", "age": 30}
+# Convert Python dictionary to JSON data sorted by key with indent level 4
+json_data_sorted = json.dumps(python_obj, sort_keys=True, indent=4)
+# print(json_data_sorted)
+
+# 5
+with open("resources\states.json", mode="r", encoding="utf-8") as read_file:
+    a = json.load(read_file)
+new_data = []
+
+for b in a["states"]:
+    b.pop("area_codes")
+
+# Write the new JSON data to a new file
+with open('results\ew_states.json', 'w') as new_file:
+    json.dump(a, new_file, indent=4)
+
+# Print the new data to verify
+print(json.dumps(a, indent=4))
